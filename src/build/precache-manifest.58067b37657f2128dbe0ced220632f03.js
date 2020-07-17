869d4ff1734f735150c1a7ad0764f76a9a645597
Kalyan Gurugubelli <kalyangurugubelli@Kalyans-MacBook-Pro.local>
2020-07-17 00:10:32 -0400

    first commit

---
+++ src/build/precache-manifest.58067b37657f2128dbe0ced220632f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4631e1c54c610fb93815666c5c561c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83b1c62fcdf0550fddf3",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "eea8f08fba08b6e482e3",
+    "url": "/static/js/2.f9197f5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f9197f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b1c62fcdf0550fddf3",
+    "url": "/static/js/main.a2d23c90.chunk.js"
+  },
+  {
+    "revision": "6f2bb5c49fb1719cfc92",
+    "url": "/static/js/runtime-main.02cb830c.js"
+  }
+]);
